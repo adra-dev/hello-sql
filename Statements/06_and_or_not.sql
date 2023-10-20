b2f757acf983066546a27e096073127089a510d8
AdraDev <alfaroar98@gmail.com>
2023-10-20 17:35:01 -0600

    Segunda clase: statements(01-07) y Tercera clase: statements,database

--- Statements/06_and_or_not.sql
+++ Statements/06_and_or_not.sql
@@ -1,5 +1,12 @@
-SELECT * FROM users WHERE not email = 'sara@gmail.com';
+/*
+NOT, AND, OR
+*/
 
-SELECT * FROM users WHERE not email = 'sara@gmail.com' AND age = 15;
+-- Obtiene todos datos de la tabla "users" con email distinto a sara@gmail.com
+SELECT * FROM users WHERE NOT email = 'sara@gmail.com';
 
-SELECT * FROM users WHERE not email = 'sara@gmail.com' OR age = 15;
+-- Obtiene todos datos de la tabla "users" con email distinto a sara@gmail.com y edad igual a 15
+SELECT * FROM users WHERE NOT email = 'sara@gmail.com' AND age = 15;
+
+-- Obtiene todos datos de la tabla "users" con email distinto a sara@gmail.com o edad igual a 15
+SELECT * FROM users WHERE NOT email = 'sara@gmail.com' OR age = 15;
